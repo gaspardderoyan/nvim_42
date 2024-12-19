682eff5a08a95de52ed339d43a9bb788dc77b806
Gaspard Deroyan <gaspard.deroyan@gmail.com>
2024-12-19 01:12:52 +0000

    changed to dracula

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -4,21 +4,30 @@ return {
         lazy = true,
         priority = 1000,
     },
-    { 
-        "catppuccin/nvim", 
-        name = "catppuccin", 
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
         priority = 1000,
-        config = function()
-            vim.cmd("colorscheme catppuccin-mocha")
-        end
+        -- config = function()
+        --     vim.cmd("colorscheme catppuccin-mocha")
+        -- end
     },
     {
-        "rose-pine/neovim", 
+        "rose-pine/neovim",
         name = "rose-pine",
         lazy = true,
         -- config = function()
         --     vim.cmd("colorscheme rose-pine-moon")
         -- end
+    },
+    {
+        "Mofiqul/dracula.nvim",
+        prioority = 1000,
+        config = function()
+            require("dracula").setup({})
+            vim.cmd("colorscheme dracula")
+        end
+
     }
 }
 
