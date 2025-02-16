a1d1b16f8cb4d8291f40798a8cf503621285a679
Gaspard Deroyan <gaspard.deroyan@gmail.com>
2025-02-16 05:34:03 +0000

    added preview to neotree

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -49,7 +49,21 @@ return {
             "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
             "MunifTanjim/nui.nvim",
             -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-        }
+        },
+		config = function()
+			require("neo-tree").setup({
+				window = {
+					mappings = {
+						["P"] = {
+							"toggle_preview",
+							config = {
+								use_float = false
+							},
+						},
+					},
+				},
+			})
+		end
     }
 }
 
