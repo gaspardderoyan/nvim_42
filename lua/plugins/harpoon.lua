2b79438c063a6b88931e6b48d4cbf49c2a42a487
Gaspard Deroyan <gaspard.deroyan@gmail.com>
2025-09-17 12:56:52 +0200

    feat: added harpoon plugin

---
+++ lua/plugins/harpoon.lua
@@ -0,0 +1,40 @@
+return {
+	{
+		"ThePrimeagen/harpoon",
+		branch = "harpoon2",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+		config = function()
+			local harpoon = require("harpoon")
+			harpoon:setup()
+			vim.keymap.set("n", "<leader>A", function()
+				harpoon:list():add()
+			end, { desc = "Add file to harpoon", silent = false })
+			vim.keymap.set("n", "<C-x>", function()
+				harpoon.ui:toggle_quick_menu(harpoon:list())
+			end, { desc = "Toggle harpoon quick menu", silent = false })
+
+			vim.keymap.set("n", "<C-i>", function()
+				harpoon:list():select(1)
+			end, { desc = "Harpoon select file 1", silent = false })
+			vim.keymap.set("n", "<C-o>", function()
+				harpoon:list():select(2)
+			end, { desc = "Harpoon select file 2", silent = false })
+			vim.keymap.set("n", "<C-p>", function()
+				harpoon:list():select(3)
+			end, { desc = "Harpoon select file 3", silent = false })
+			vim.keymap.set("n", "<C-[>", function()
+				harpoon:list():select(4)
+			end, { desc = "Harpoon select file 4", silent = false })
+
+			-- Toggle previous & next buffers stored within Harpoon list
+			vim.keymap.set("n", "<C-S-P>", function()
+				harpoon:list():prev()
+			end, { desc = "Harpoon previous buffer", silent = false })
+			vim.keymap.set("n", "<C-S-N>", function()
+				harpoon:list():next()
+			end, { desc = "Harpoon next buffer", silent = false })
+		end,
+	},
+}
