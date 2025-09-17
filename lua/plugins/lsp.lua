1fd712f06293122dd9a3c4604523dd3a9d518914
Gaspard Deroyan <gaspard.deroyan@gmail.com>
2025-09-17 12:56:20 +0200

    feat: updated lsp settings for css & js/ts

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -196,8 +196,7 @@ return {
 			--  - settings (table): Override the default settings passed when initializing the server.
 			--        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
 			local servers = {
-				-- clangd = {},
-				-- gopls = {},
+				-- PYTHON
 				pyright = {
 					plugins = {
 						pycodestyle = {
@@ -206,16 +205,8 @@ return {
 						},
 					},
 				},
-				-- rust_analyzer = {},
-				-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-				--
-				-- Some languages (like typescript) have entire language plugins that can be useful:
-				--    https://github.com/pmizio/typescript-tools.nvim
-				--
-				-- But for many setups, the LSP (`ts_ls`) will work just fine
-				-- ts_ls = {},
-				--
 
+				-- LUA
 				lua_ls = {
 					-- cmd = { ... },
 					-- filetypes = { ... },
@@ -228,6 +219,20 @@ return {
 							-- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
 							-- diagnostics = { disable = { 'missing-fields' } },
 						},
+					},
+				},
+
+				-- CSS
+				cssls = {},
+
+				vtsls = {
+					filetypes = {
+						"javascript",
+						"javascriptreact",
+						"javascript.jsx",
+						"typescript",
+						"typescriptreact",
+						"typescript.tsx",
 					},
 				},
 			}
@@ -248,6 +253,9 @@ return {
 			local ensure_installed = vim.tbl_keys(servers or {})
 			vim.list_extend(ensure_installed, {
 				"stylua", -- Used to format Lua code
+				"vtsls", -- TS/JS language server
+				"eslint-lsp", -- ESLint LSP
+				"prettierd", -- Prettier daemon (or "biome" if you prefer Biome)
 			})
 			require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
@@ -497,6 +505,11 @@ return {
 				"vimdoc",
 				"gofumpt",
 				"gopls",
+				"javascript",
+				"typescript",
+				"tsx",
+				"json",
+				"css",
 			},
 			-- Autoinstall languages that are not installed
 			auto_install = true,
